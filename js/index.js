7511c93c6ffb4517feff88eb205435e574181dbd
Simon_Paul <simonchasnovsky@gmail.com>
2023-10-12 13:58:24 -0300

    Tech list style fix, close popups on window click

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-const inlineWordContainer = document.querySelectorAll(".inline-word-container"),
-inlineWordChildren = document.querySelectorAll(".inline-word > div");
+// Get index subtitles and their replacements from DOM
+const inlineWordContainer = document.querySelectorAll(".inline-word-container");
+const inlineWordChildren = document.querySelectorAll(".inline-word > div");
 
+// Replace the index subtitles on hover
 inlineWordContainer.forEach(function(container) {
-
     container.addEventListener("mouseenter", function() {
         container.classList.add("inline-word-container--active");
 
@@ -11,15 +12,15 @@ inlineWordContainer.forEach(function(container) {
                 container.classList.remove("inline-word-container--active");
             }, 1600);
         });
-
     });
 });
 
-const inlineWordContainerJp = document.querySelectorAll(".inline-word-container"),
-inlineWordChildrenJp = document.querySelectorAll(".inline-word-jp > div");
+// Get index japanese subtitle and its replacement from DOM
+const inlineWordContainerJp = document.querySelectorAll(".inline-word-container");
+const inlineWordChildrenJp = document.querySelectorAll(".inline-word-jp > div");
 
+// Replace the japanese index subtitle on hover
 inlineWordContainerJp.forEach(function(container) {
-
     container.addEventListener("mouseenter", function() {
         container.classList.add("inline-word-container--active");
 
@@ -28,29 +29,34 @@ inlineWordContainerJp.forEach(function(container) {
                 container.classList.remove("inline-word-container--active");
             }, 1600);
         });
-
     });
 });
 
-const btnBlog = document.querySelectorAll(".blog-btn"), 
-    blog = document.querySelector(".blog-pu"),
-    btnPortfolio = document.querySelectorAll(".portfolio-btn"),
-    portfolio = document.querySelector(".portfolio-pu"),
-    btnAmbient = document.querySelectorAll(".ambient-btn"),
-    ambient = document.querySelector(".ambient-pu"),
-    btnMixtorrents = document.querySelectorAll(".mixtorrents-btn"),
-    mixtorrents = document.querySelector(".mixtorrents-pu");
+// Get project popups
+const blog = document.querySelector(".blog-pu");
+const portfolio = document.querySelector(".portfolio-pu");
+const ambient = document.querySelector(".ambient-pu");
+const mixtorrents = document.querySelector(".mixtorrents-pu");
 
-const socialsOpen = document.querySelector(".left-bar-socials"),
-    socialsClose = document.querySelector(".socials-closer"),
-    socials = document.querySelector(".socials-container"),
-    aboutOpen = document.querySelector(".left-bar-about"),
-    aboutClose = document.querySelector(".about-closer"),
-    about = document.querySelector(".about-container");
+// Get projects buttons
+const btnBlog = document.querySelectorAll(".blog-btn");
+const btnPortfolio = document.querySelectorAll(".portfolio-btn");
+const btnAmbient = document.querySelectorAll(".ambient-btn");
+const btnMixtorrents = document.querySelectorAll(".mixtorrents-btn");
 
-let socialsShow = false,
-    aboutShow = false;
+// Get socials and about popups and buttons
+const socials = document.querySelector(".socials-container");
+const socialsOpen = document.querySelector(".left-bar-socials");
+const socialsClose = document.querySelector(".socials-closer");
+const about = document.querySelector(".about-container");
+const aboutOpen = document.querySelector(".left-bar-about");
+const aboutClose = document.querySelector(".about-closer");
 
+// Set initial socials and about popups statuses
+let socialsShow = false;
+let aboutShow = false;
+
+// For the open and close buttons, open or close the project popup
 btnBlog.forEach(btn => {
     btn.addEventListener("click", () => {
         blog.classList.toggle("blog-pu--active");
@@ -75,6 +81,7 @@ btnMixtorrents.forEach(btn => {
     });
 });
 
+// Open about popup
 aboutOpen.addEventListener("click", () => {
     if (socialsShow == true) {
         socials.classList.remove("socials-pu--active");
@@ -90,6 +97,7 @@ aboutOpen.addEventListener("click", () => {
     }
 });
 
+// Open socials popup
 socialsOpen.addEventListener("click", () => {
     if (aboutShow == true) {
         about.classList.remove("about-pu--active");
@@ -105,12 +113,55 @@ socialsOpen.addEventListener("click", () => {
     }
 });
 
+// Close about popup
 aboutClose.addEventListener("click", () => {
     about.classList.remove("about-pu--active");
     aboutShow = false;
 });
 
+// Close socials popup
 socialsClose.addEventListener("click", () => {
     socials.classList.remove("socials-pu--active");
     socialsShow = false;
+});
+
+// Close any open popup on click of the window
+window.addEventListener('click', function (event) {
+    if (!(event.target.closest(".socials-container")) && !(event.target.closest(".left-bar-socials")) && !(event.target.closest(".socials-closer"))) {
+        if (socials.classList.contains("socials-pu--active")) {
+            socials.classList.remove("socials-pu--active");
+            socialsShow = false;
+        }
+    }
+
+    if (!(event.target.closest(".about-container")) && !(event.target.closest(".left-bar-about")) && !(event.target.closest(".about-closer"))) {
+        if (about.classList.contains("about-pu--active")) {
+            about.classList.remove("about-pu--active");
+            aboutShow = false;
+        }
+    }
+
+    if (!(event.target.closest(".portfolio-btn")) && !(event.target.closest(".portfolio-pu"))) {
+        if (portfolio.classList.contains("portfolio-pu--active")) {
+            portfolio.classList.toggle("portfolio-pu--active");
+        }
+    }
+
+    if (!(event.target.closest(".ambient-btn")) && !(event.target.closest(".ambient-pu"))) {
+        if (ambient.classList.contains("ambient-pu--active")) {
+            ambient.classList.toggle("ambient-pu--active");
+        }
+    }
+
+    if (!(event.target.closest(".blog-btn")) && !(event.target.closest(".blog-pu"))) {
+        if (blog.classList.contains("blog-pu--active")) {
+            blog.classList.toggle("blog-pu--active");
+        }
+    }
+
+    if (!(event.target.closest(".mixtorrents-btn")) && !(event.target.closest(".mixtorrents-pu"))) {
+        if (mixtorrents.classList.contains("mixtorrents-pu--active")) {
+            mixtorrents.classList.toggle("mixtorrents-pu--active");
+        }
+    }
 });
